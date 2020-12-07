6c8a8993e9680f5aafb35b3c33b29167e2bedea2
Salim Bakri <saleem.bakri94@gmail.com>
2020-12-06 22:24:01 -0500

    add few unit tests

--- src/app/display/display.component.spec.ts
+++ src/app/display/display.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AppState } from '../app.state';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { DisplayComponent } from './display.component';
+import * as ImagesActions from '../store/actions/images.actions';
+import { By } from '@angular/platform-browser';
 
 describe('DisplayComponent', () => {
   let component: DisplayComponent;
   let fixture: ComponentFixture<DisplayComponent>;
+  let store: MockStore<AppState>;
+  const initialState = {
+    counter: 0,
+    info: [
+      {
+        name: 'A',
+        size: '38 KB',
+        format: 'jpeg',
+        date: '2020-12-06',
+        dataURL: 'A.jpeg'
+      },
+      {
+        name: 'B',
+        size: '100 KB',
+        format: 'png',
+        date: '2019-11-21',
+        dataURL: 'B.png'
+      },
+    ]
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DisplayComponent ]
+      declarations: [ DisplayComponent ],
+      imports: [],
+      providers: [provideMockStore({ initialState })]
     })
     .compileComponents();
   }));
@@ -16,10 +41,25 @@ describe('DisplayComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DisplayComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
+    spyOn(store, 'dispatch').and.callFake(() => {});
   });
 
-  it('should create', () => {
+  it('should create the display component with its initial state', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update the UI when deleting images', () => {
+    store.dispatch(new ImagesActions.RemoveImage(1));
+    store.dispatch(new ImagesActions.RemoveImage(2));
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('.dc-content')).length).toBe(0);
+  });
+
+  it('should dispatch delete action when delete function is invoked', () => {
+    const row = 1;
+    component.delete(1);
+    expect(store.dispatch).toHaveBeenCalled();
   });
 });
--- src/app/upload/upload.component.spec.ts
+++ src/app/upload/upload.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UploadComponent } from './upload.component';
+import * as ImagesActions from '../store/actions/images.actions';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { AppState } from '../app.state';
+
+
 
 describe('UploadComponent', () => {
   let component: UploadComponent;
   let fixture: ComponentFixture<UploadComponent>;
+  let store: MockStore<AppState>;
 
+  const initialState = {
+    counter: 0,
+    info: [1]
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UploadComponent ]
+      declarations: [ UploadComponent ],
+      imports: [],
+      providers: [provideMockStore({ initialState })]
     })
     .compileComponents();
   }));
@@ -16,10 +27,38 @@ describe('UploadComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UploadComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
+    spyOn(store, 'dispatch').and.callFake(() => { });
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should validate file format to be an image', () => {
+    const textFile = {
+      lastModified: 1438583972000,
+      lastModifiedDate: '2015-08-03T06:39:32.000Z',
+      name: 'readme.txt',
+      size: 236,
+      type: 'text/plain',
+      relativePath: './readme.txt',
+      data: 'data:text/plain;base64,DQojaWdub3JlIHRodW1ibmFpbHMgY3JlYXRlZCBieSB3aW5kb3dz…xoDQoqLmJhaw0KKi5jYWNoZQ0KKi5pbGsNCioubG9nDQoqLmRsbA0KKi5saWINCiouc2JyDQo='
+    };
+    expect(component.isFormatAllowed(textFile.name)).toBe(false);
+  });
+
+  it('should modify storage state with adding a new image', () => {
+    const newImg = {
+      name: 'C',
+      size: '70 KB',
+      format: 'gif',
+      date: '2021-1-1',
+      dataURL: 'C.gif'
+    };
+    store.dispatch(new ImagesActions.AddImage(newImg));
+    expect(store.dispatch).toHaveBeenCalled();
+  });
 });
+
